bf3cfb338e8d35215338c8e3bd85af4fb8ca2adf
Nikita Bogoslovskiy <nikita-beeline@yandex.ru>
2024-11-20 14:37:21 +0300

    add modes and cuda support

--- yolo/services/yolov8n/scripts/run.sh
+++ yolo/services/yolov8n/scripts/run.sh
@@ -1,8 +1,13 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
-if [ $MODE = "TRAIN" ]; then
-    yolo detect train model=yolov8n.pt data="/app/dataset/data.yaml" epochs=20 imgsz=640 device='cpu' project="/app/output"
+MODE=$1
+if [ "$MODE" = "train" ]; then
+    echo "train"
+    # yolo detect train model=yolov8n.pt data="/app/dataset/data.yaml" epochs=20 imgsz=640 device='cpu' project="/app/output"
     # yolo export model="/app/output/weights/best.pt" format=tflite simplify half
+elif [ "$MODE" = "test" ]; then
+    echo "test"
 else
     echo "Undefined mode"
 fi
+sleep infinity
