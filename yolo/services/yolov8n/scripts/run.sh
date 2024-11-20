ffc9a4ef96dd81287633f568dd36fc50567c8bc1
Nikita Bogoslovskiy <nikita-beeline@yandex.ru>
2024-11-20 17:51:05 +0300

    update docker run

--- yolo/services/yolov8n/scripts/run.sh
+++ yolo/services/yolov8n/scripts/run.sh
@@ -2,8 +2,11 @@
 
 MODE=$1
 if [ "$MODE" = "train" ]; then
-    echo "train"
-    # yolo detect train model=yolov8n.pt data="/app/dataset/data.yaml" epochs=20 imgsz=640 device='cpu' project="/app/output"
+    echo -e "Start training"
+    echo -e "\n"
+    yolo detect train model=yolov8n.pt data="/app/dataset/data.yaml" epochs=1 imgsz=640 device=0 project="/app/output"
+    echo -e "\n"
+    echo -e "Finish training"
     # yolo export model="/app/output/weights/best.pt" format=tflite simplify half
 elif [ "$MODE" = "test" ]; then
     echo "test"
